620ad912367c013428131184f11a0d5106c2c660
Yuxuan1998 <hanyuxuan19@gmail.com>
2025-01-17 07:52:45 +0800

    update download

--- modepd/download.py
+++ modepd/download.py
@@ -6,9 +6,10 @@ from huggingface_hub import snapshot_download
 models = [
     # "deepseek-ai/DeepSeek-V2.5-1210", 
     # 'deepseek-ai/DeepSeek-V2-Lite', 
-    # 'deepseek-ai/DeepSeek-V2-Lite-Chat', 
-    'Qwen/Qwen2.5-3B', 
+    'deepseek-ai/DeepSeek-V2-Lite-Chat', 
+    # 'Qwen/Qwen2.5-3B', 
     # 'Qwen/Qwen2.5-3B-Instruct',
+    # "Qwen/Qwen2.5-0.5B-Instruct"
 ]
 for model in models:
     snapshot_download(model)
