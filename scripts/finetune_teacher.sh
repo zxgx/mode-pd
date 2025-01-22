a9b4214f649f44b516a9357220d32b30e3ec9ae5
Yuxuan1998 <hanyuxuan19@gmail.com>
2025-01-22 16:02:00 +0800

    grid search

--- scripts/finetune_teacher.sh
+++ scripts/finetune_teacher.sh
@@ -51,23 +51,54 @@ echo "detected hosts: $(cat $HOSTFILE)"
 # =============== launch cmds ================
 image=${SCRATCH}/images/cuda_12.4.1-cudnn-devel-u22.04.sif
 
-mpirun --hostfile $HOSTFILE --np $NNODES -N 1 \
-    singularity exec --bind $SCRATCH:$SCRATCH --nv $image \
-    /bin/bash -c "source $SCRATCH/venvs/modepd/bin/activate && \
-    export PYTHONPATH=$PBS_O_WORKDIR:\$PYTHONPATH && \
-    torchrun \
-    --nproc_per_node=$GPUS_PER_NODE \
-    --nnodes=$NNODES \
-    --node_rank=\${OMPI_COMM_WORLD_RANK} \
-    --master_addr=$MASTER_ADDR \
-    --master_port=$MASTER_PORT \
-    modepd/train.py \
-    --output_dir deepseek_v2_10B_finetune\
-    --model_name_or_path deepseek-ai/DeepSeek-V2-Lite-Chat \
-    --with_tracking \
-    --max_train_steps 819 > eval.log 2>&1
-"
+# =============== grid search parameters ================
+BLOCK_SIZE=1024
 
-# for debug purpose
-# torchrun --standalone --nproc_per_node 2 modepd/train.py --with_tracking --output_dir logs/demo
+TRAINING_TOKEN_B_VALUES=(0.1 0.5 1 2)
+WEIGHT_DECAY_VALUES=(0.1 0.01 0.001)
+LEARNING_RATE_VALUES=(2.4e-4 1e-4 5e-5)
+LR_SCHEDULER_TYPES=("linear" "cosine" "polynomial")
+NUM_WARMUP_STEPS_VALUES=(0 100 200)
+
+# =============== grid search ================
+for TRAINING_TOKEN_B in "${TRAINING_TOKEN_B_VALUES[@]}"; do
+    # calculate steps
+    MAX_TRAIN_STEPS= $(echo "scale=0; ($TRAINING_TOKEN_B * 2 ** 30) / $BLOCK_SIZE / $NNODES / $GPUS_PER_NODE" | bc)
+
+    for WEIGHT_DECAY in "${WEIGHT_DECAY_VALUES[@]}"; do
+        for LEARNING_RATE in "${LEARNING_RATE_VALUES[@]}"; do
+            for LR_SCHEDULER_TYPE in "${LR_SCHEDULER_TYPES[@]}"; do
+                for NUM_WARMUP_STEPS in "${NUM_WARMUP_STEPS_VALUES[@]}"; do
+                    echo "Running with $TRAINING_TOKEN_B B tokens, weight_decay=$WEIGHT_DECAY, learning_rate=$LEARNING_RATE, lr_scheduler_type=$LR_SCHEDULER_TYPE, num_warmup_steps=$NUM_WARMUP_STEPS"
+
+                    mpirun --hostfile $HOSTFILE --np $NNODES -N 1 \
+                        singularity exec --bind $SCRATCH:$SCRATCH --nv $image \
+                        /bin/bash -c "source $SCRATCH/venvs/modepd/bin/activate && \
+                        export PYTHONPATH=$PBS_O_WORKDIR:\$PYTHONPATH && \
+                        torchrun \
+                        --nproc_per_node=$GPUS_PER_NODE \
+                        --nnodes=$NNODES \
+                        --node_rank=\${OMPI_COMM_WORLD_RANK} \
+                        --master_addr=$MASTER_ADDR \
+                        --master_port=$MASTER_PORT \
+                        modepd/train.py \
+                        --output_dir logs/teacher_${TRAINING_TOKEN_B}_${WEIGHT_DECAY}_${LEARNING_RATE}_${LR_SCHEDULER_TYPE}_${NUM_WARMUP_STEPS} \
+                        --model_name_or_path deepseek-ai/DeepSeek-V2-Lite-Chat \
+                        --with_tracking \
+                        --weight_decay=$WEIGHT_DECAY \
+                        --learning_rate=$LEARNING_RATE \
+                        --lr_scheduler_type=$LR_SCHEDULER_TYPE \
+                        --num_warmup_steps=$NUM_WARMUP_STEPS \
+                        --block_size=$BLOCK_SIZE\
+                        --max_train_steps=$MAX_TRAIN_STEPS > eval_${TRAINING_TOKEN_B}_${WEIGHT_DECAY}_${LEARNING_RATE}_${LR_SCHEDULER_TYPE}_${NUM_WARMUP_STEPS}.log 2>&1
+                    "
+
+                    # for debug purpose
+                    # torchrun --standalone --nproc_per_node 2 modepd/train.py --with_tracking --output_dir logs/demo
+                done
+            done
+        done
+    done
+done
+
 rm $HOSTFILE
