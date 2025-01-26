b66301a1b123377ade240d60e7392544d9043abf
zxgx <zhangg1998@outlook.com>
2025-01-26 13:20:21 +0800

    init version of pruning

--- modepd/prune.py
+++ modepd/prune.py
@@ -1,29 +1,77 @@
 import argparse
 import torch
+from torch.utils.data import DataLoader
 from transformers import (
     AutoTokenizer, AutoModelForCausalLM, GenerationConfig,
+    DataCollatorForLanguageModeling
 )
+
+from modepd.utils import build_dataset
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name_or_path", type=str, default="deepseek-ai/DeepSeek-V2-Lite-Chat",)
+    parser.add_argument("--dataset_name", type=str, default="HuggingFaceFW/fineweb",)
+    parser.add_argument("--dataset_config_name", type=str, default="sample-350BT",)
+    parser.add_argument("--block_size", type=int, default=4*1024,)
 
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+    device = torch.cuda.current_device()
     model_name = args.model_name_or_path
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(
         model_name, trust_remote_code=True, torch_dtype=torch.bfloat16, use_cache=False)
     
-    import pdb; pdb.set_trace()
-
     if "DeepSeek-V2" in model_name:
         model.generation_config = GenerationConfig.from_pretrained(model_name)
         model.generation_config.pad_token_id = model.generation_config.eos_token_id
+    
+    model.eval()
 
+    import pdb
+    pdb.set_trace()
+
+    train_dataset = build_dataset(args, tokenizer)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    train_dataloader = DataLoader(
+        train_dataset,
+        collate_fn=data_collator,
+        batch_size=1,
+        num_workers=4,
+    )
+    for batch in train_dataloader:
+        print(f"{batch['input_ids'].shape}")
+        break
+
+    num_layers = model.config.num_hidden_layers
+    similarities = torch.full(num_layers, num_layers, -float('inf'), device=device)
+    cache, handles = [], []
+
+    # register hooks which save the input for each layer and the output of the last layer
+    for i in range(num_layers):
+        layer = model.model.layers[i]
+        
+        def stateful_hook(module, _input, _output):
+            cache.append([])
+
+            cache[i].append(_input.to('cpu', non_blocking=True))
+
+            if i == num_layers-1:
+                cache.append([])
+                cache[i+1].append(_output.to('cpu', non_blocking=True))
+
+        handle = layer.register_forward_hook(stateful_hook)
+        handles.append(handle)
+    
+    # clear handles before saving
+    for handle in handles:
+        handle.remove()
+
+        
 
 if __name__ == "__main__":
     main()
--- modepd/train.py
+++ modepd/train.py
@@ -2,7 +2,6 @@
 Reference: https://github.com/huggingface/transformers/blob/main/examples/pytorch/language-modeling/run_clm_no_trainer.py
 """
 import argparse
-from itertools import chain
 import logging
 import os
 from tqdm.auto import tqdm
@@ -14,19 +13,14 @@ from accelerate.logging import get_logger
 from accelerate.utils import set_seed
 
 import datasets
-from datasets import (
-    load_dataset, 
-    # interleave_datasets,
-)
 import transformers
 from transformers import (
     AutoTokenizer, AutoModelForCausalLM, GenerationConfig,
-    # DataCollatorForLanguageModeling,
-    default_data_collator,
+    DataCollatorForLanguageModeling,
     get_scheduler,
 )
 
-from modepd.utils import get_memory_stats
+from modepd.utils import get_memory_stats, build_dataset
 
 logger = get_logger(__name__)
 
@@ -134,70 +128,7 @@ def main():
     if len(tokenizer) > embedding_size:
         assert False, f"Tokenizer vocab size {len(tokenizer)} is larger than the model embedding size {embedding_size}."
     
-    #################
-    # Prepare dataset
-    # TODO: mix fineweb (en) and fineweb-2 (zh) dataset
-    # en = load_dataset("HuggingFaceFW/fineweb", name="sample-350BT", split="train", streaming=True)
-    # zh = load_dataset("HuggingFaceFW/fineweb-2", name="cmn_Hani", split="train", streaming=True)
-    # ds = interleave_datasets([en, zh], probabilities=[0.8, 0.2], seed=42)
-    # ds = en
-    raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name, streaming=True)
-    
-    #################
-    # Preprocessing the datasets.
-    # 1. Only load text fields for the dataloader
-    column_names = raw_datasets["train"].column_names
-    text_column_name = "text" if "text" in column_names else column_names[0]
-
-    def tokenize_function(examples):
-        return tokenizer(examples[text_column_name])
-
-    with accelerator.main_process_first():
-        tokenized_datasets = raw_datasets.map(
-            tokenize_function,
-            batched=True,
-            remove_columns=column_names,
-        )
-
-    # 2. Padding to max length    
-    if args.block_size is None:
-        block_size = tokenizer.model_max_length
-    else:
-        if args.block_size > tokenizer.model_max_length:
-            logger.warning(
-                f"The block_size passed ({args.block_size}) is larger than the maximum length for the model "
-                f"({tokenizer.model_max_length}). Using block_size={tokenizer.model_max_length}."
-            )
-        block_size = min(args.block_size, tokenizer.model_max_length)
-
-    # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
-    def group_texts(examples):
-        # Concatenate all texts.
-        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
-        # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
-        total_length = (total_length // block_size) * block_size
-        # Split by chunks of max_len.
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
-        result["labels"] = result["input_ids"].copy()
-        return result
-
-    with accelerator.main_process_first():
-        lm_datasets = tokenized_datasets.map(
-            group_texts,
-            batched=True,
-        )
-    
-    train_dataset = lm_datasets["train"]
-    # fine-web doesn't have validation set
-    # eval_dataset = lm_datasets["validation"]
-    # this logic will be handled by `accelerator.prepare`
-    # if accelerator.num_processes > 1:
-    #     train_dataset = split_dataset_by_node(train_dataset, rank=accelerator.process_index, world_size=accelerator.num_processes)
+    train_dataset = build_dataset(args, tokenizer, logger, accelerator)
 
     # Log a few random samples from the training set:
     data_iter = iter(train_dataset)
@@ -206,11 +137,11 @@ def main():
         logger.info(f"rank: {accelerator.process_index}/{accelerator.num_processes} sample {index}: {sample['input_ids'][:5]}", main_process_only=False)
     
     # 3. DataLoaders creation
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
     train_dataloader = DataLoader(
         train_dataset,
         # shuffle=True,
-        # collate_fn=DataCollatorForLanguageModeling(tokenizer, mlm=False),
-        collate_fn=default_data_collator,
+        collate_fn=data_collator,
         batch_size=args.per_device_train_batch_size,
     )
 
