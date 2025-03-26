411030f95c02bbffdf0d296727516f19e9e541fe
zxgx <zhangg1998@outlook.com>
2025-03-26 18:24:43 +0800

    update pruning scripts

--- scripts/expert_prune/mone-layer-rs-out-fusion.sh
+++ scripts/expert_prune/mone-layer-rs-out-fusion.sh
@@ -11,33 +11,33 @@ model_id="OLMoE-1B-7B-0125"
 python modepd/prune.py --model_name_or_path /mnt/videodata/zhgeng/models/$model_id $data_config \
     --expert_prune --preserve_n_experts 48 --expert_ranking_scope layer \
     --expert_prune_metric mone --mone_ranking_metric fusion \
-    --compressed_model_save_path exp/mone_fusion_layer/$model_id-mone-pruned-48
+    --compressed_model_save_path exp/mone_rs_out_fusion_layer/$model_id-mone-pruned-48
 
 python modepd/prune.py --model_name_or_path /mnt/videodata/zhgeng/models/$model_id $data_config \
     --expert_prune --preserve_n_experts 32 --expert_ranking_scope layer \
     --expert_prune_metric mone --mone_ranking_metric fusion \
-    --compressed_model_save_path exp/mone_fusion_layer/$model_id-mone-pruned-32
+    --compressed_model_save_path exp/mone_rs_out_fusion_layer/$model_id-mone-pruned-32
 
 model_id="Moonlight-16B-A3B"
 
 python modepd/prune.py --model_name_or_path /mnt/videodata/zhgeng/models/$model_id $data_config \
     --expert_prune --preserve_n_experts 48 --expert_ranking_scope layer \
     --expert_prune_metric mone --mone_ranking_metric fusion \
-    --compressed_model_save_path exp/mone_fusion_layer/$model_id-mone-pruned-48
+    --compressed_model_save_path exp/mone_rs_out_fusion_layer/$model_id-mone-pruned-48
 
 python modepd/prune.py --model_name_or_path /mnt/videodata/zhgeng/models/$model_id $data_config \
     --expert_prune --preserve_n_experts 32 --expert_ranking_scope layer \
     --expert_prune_metric mone --mone_ranking_metric fusion \
-    --compressed_model_save_path exp/mone_fusion_layer/$model_id-mone-pruned-32
+    --compressed_model_save_path exp/mone_rs_out_fusion_layer/$model_id-mone-pruned-32
 
 model_id="DeepSeek-V2-Lite"
 
 python modepd/prune.py --model_name_or_path /mnt/videodata/zhgeng/models/$model_id $data_config \
     --expert_prune --preserve_n_experts 48 --expert_ranking_scope layer \
     --expert_prune_metric mone --mone_ranking_metric fusion \
-    --compressed_model_save_path exp/mone_fusion_layer/$model_id-mone-pruned-48
+    --compressed_model_save_path exp/mone_rs_out_fusion_layer/$model_id-mone-pruned-48
 
 python modepd/prune.py --model_name_or_path /mnt/videodata/zhgeng/models/$model_id $data_config \
     --expert_prune --preserve_n_experts 32 --expert_ranking_scope layer \
     --expert_prune_metric mone --mone_ranking_metric fusion \
-    --compressed_model_save_path exp/mone_fusion_layer/$model_id-mone-pruned-32
+    --compressed_model_save_path exp/mone_rs_out_fusion_layer/$model_id-mone-pruned-32
